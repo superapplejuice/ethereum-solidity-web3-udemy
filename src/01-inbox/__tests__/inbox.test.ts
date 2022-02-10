050042c3111499d478b35cbd77cc25ba28fa96cd
Jan Mathew <loveless_wawa@hotmail.com>
2022-02-10 13:51:22 +0800

    update imports

--- src/01-inbox/__tests__/inbox.test.ts
+++ src/01-inbox/__tests__/inbox.test.ts
@@ -1,26 +1,14 @@
 import ganache from 'ganache';
 import Web3 from 'web3';
-import getContractData from '../compile';
 
-const GAS_AMOUNT = 1000000;
-const DEFAULT_MESSAGE = 'hello world';
+import { DEFAULT_MESSAGE } from '../consts';
+import createInboxContract from '../compile';
 
 const setupContract = async () => {
   const provider = ganache.provider();
   const web3 = new Web3(provider as any);
 
-  // Get list of accounts
-  const accounts = await web3.eth.getAccounts();
-
-  const contractData = await getContractData();
-  const newContract = new web3.eth.Contract(JSON.parse(contractData.interface));
-
-  // Use one of the accounts to deploy the contract
-  const inboxContract = await newContract
-    .deploy({ data: contractData.bytecode, arguments: [DEFAULT_MESSAGE] })
-    .send({ from: accounts[0], gas: GAS_AMOUNT });
-
-  return { accounts, inboxContract };
+  return createInboxContract(web3);
 };
 
 afterEach(jest.clearAllMocks);
