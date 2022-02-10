8b5dc300d6e562198e26487411876b2ddb60c6fc
Jan Mathew <loveless_wawa@hotmail.com>
2022-02-10 13:51:31 +0800

    move constants to file

---
+++ src/01-inbox/consts.ts
@@ -0,0 +1,7 @@
+export const GAS_AMOUNT = 1000000;
+export const DEFAULT_MESSAGE = 'hello world';
+
+export const CRYPTO_MNEMONIC =
+  'spell nasty history park cloth person tunnel vendor exercise comfort water despair';
+export const RINKEBY_NETWORK_URL =
+  'https://rinkeby.infura.io/v3/954c7d9e12d24d0ca3311f52f2645bb6';
